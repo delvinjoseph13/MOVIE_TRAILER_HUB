34a40e46a853f545a1fb86d61fa3e2aa522d8f07
delvin <delvinjoseph221@gmail.com>
2023-07-30 20:07:32 +0530

    almost over

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import React from 'react'
 import Navbar from './components/navbar/Navbar';
-import "./App.css"
+import "./App.css";
+import {action,originals,comdey,horror,action2,Documentaries} from './urls'
 import Banner from './components/Banner/Banner';
 import RowPoster from './components/RowPoster/RowPoster';
 function App() {
@@ -9,8 +10,15 @@ function App() {
     <div className="App">
      <Navbar/>
      <Banner/>
-     <RowPoster  title='Netflix originals'/>
-     <RowPoster  title='Action' isSmall={true}/>
+     <RowPoster url={originals}  title='Netflix originals'/>
+     <RowPoster url={action}  title='Action' isSmall={true}/>
+     <RowPoster url={comdey} title='Comdey' isSmall={true}/>
+     <RowPoster url={horror}  title='Horror' isSmall={true}/>
+     <RowPoster url={action2}  title='Romance' isSmall={true}/>
+     <RowPoster url={Documentaries}  title='Documentaries' isSmall={true}/>
+
+
+
    
     </div>
   );
--- src/components/RowPoster/RowPoster.js
+++ src/components/RowPoster/RowPoster.js
@@ -1,17 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import './RowPoster.css'
-import {API_KEY,imageUrl} from '../../constants/constants'
+import YouTube from 'react-youtube';
+import {imageUrl,API_KEY} from '../../constants/constants';
+
 import axios  from '../../axios'
 function RowPoster(props) {
   const [movies, setMovies] = useState([])
+  const [urlid,setUrlId]=useState('')
   useEffect(() => {
-    axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then((response)=>
+    axios.get(props.url).then((response)=>
     {
       console.log(response.data)
       setMovies(response.data.results)
     })
   }, [])
-  
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+    autoplay:1
+    },
+  };
+  const handleMovie = (id)=>{
+    console.log(id)
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&languge=en-US`).then((response=>{
+      if(response.data.results.length!==0){
+        setUrlId(response.data.results[0])
+      }
+      else{
+      console.log(response.data)
+      }
+    }))
+  }
+ 
   return (
     <div className='row'>
 
@@ -19,12 +42,12 @@ function RowPoster(props) {
 
         <div className="posters">
         {movies.map((obj)=>
-            <img className='posterimg' src={`${imageUrl+obj.backdrop_path}`} alt="title" />
+            <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallposter' : 'posterimg'} src={`${imageUrl+obj.backdrop_path}`} alt="title" />
 
    )}
 
         </div>
-
+       {urlid && <YouTube opts={opts} videoId={urlid.key} /> }
     </div>
   )
 }
